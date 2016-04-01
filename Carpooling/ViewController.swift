52bfe50436a4ac901347b079ddbf53019f78b44b
ss42 <https://github.com/ss42>
2016-04-01 07:39:28 -0700

    UI descriptions updated

--- Carpooling/ViewController.swift
+++ Carpooling/ViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet var background: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 }
 
