70bf7dbbc636e41eab022b5d32b588c0b0b539a5
ss42 <https://github.com/ss42>
2016-04-10 23:45:20 -0700

    Creating new ride added and datepicker fixed

--- Carpooling/RequestRideViewController.swift
+++ Carpooling/RequestRideViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 
 class RequestRideViewController: UIViewController {
 
+    @IBOutlet weak var notes: UITextView!
+
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var fromStreetAddressTextField: UITextField!
+    @IBOutlet weak var fromCityTextField: UITextField!
+    @IBOutlet weak var fromStateTextField: UITextField!
+    @IBOutlet weak var fromZipCode: UITextField!
+    
+    @IBOutlet weak var toStreetAddressTextField: UITextField!
+    @IBOutlet weak var toCityTextfield: UITextField!
+    @IBOutlet weak var toStateTextField: UITextField!
+    @IBOutlet weak var toZipCodeTextField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        datePicker.hidden = true
+        doneButton.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +41,80 @@ class RequestRideViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func fromStreetAddressTapped(sender: AnyObject) {
+        changeResponder(fromStreetAddressTextField)
     }
-    */
+    
+    @IBAction func fromCityTapped(sender: AnyObject) {
+        
+        changeResponder(fromCityTextField)
+    }
+    
+    
+    @IBAction func fromStateTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func fromZipcodeTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func toStreetAddressTapped(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func toCityTapped(sender: AnyObject) {
+        
+    }
+    
+    
+    @IBAction func toStateTapped(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func toZipcodeTapped(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func chooseDateAndTimeTapped(sender: AnyObject) {
+        datePicker.hidden = false
+        doneButton.hidden = false
+        notes.hidden = true
+     
+        //need to reload data
+        
+        
+    }
+  
+    @IBAction func doneTapped(sender: AnyObject) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        let strDate = dateFormatter.stringFromDate(datePicker.date)
+        dateLabel.text = strDate
+        datePicker.hidden = true
+        notes.hidden = false
+    }
+    
+    //to display alert for errors
+    func displayMyAlertMessage(title: String, message: String) {
+        let myAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        myAlert.addAction(okAction)
+        self.presentViewController(myAlert, animated: true, completion: nil);
+    }
+    
+    func changeResponder(textField: UITextField){
+        if textField.isFirstResponder(){
+            textField.resignFirstResponder()
+            print("dismiss respone")
+        }
+        else{
+            textField.becomeFirstResponder()
+            print("become first repsonder")
+        }
+    }
+    
+    
+    
+
 
 }
