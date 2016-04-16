333e2de0b823e430e292545d55c90bf8c5bcf0c0
ss42 <https://github.com/ss42>
2016-04-16 08:07:03 -0700

    profileviewcontroller

--- Carpooling/ProfileViewController.swift
+++ Carpooling/ProfileViewController.swift
@@ -9,20 +9,51 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
     
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var email: UITextField!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        
+        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+ 
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelTapped(sender: AnyObject) {
+        performCustomSegue()
+    }
+    
+    
+    @IBAction func SaveTapped(sender: AnyObject) {
+        performCustomSegue()
+    }
+    
+    
+    func performCustomSegue(){
+        
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        //let vc: UINavigationController = storyboard.instantiateViewControllerWithIdentifier("navView") as! UINavigationController
+        
+        let vc: UIViewController = storyboard.instantiateViewControllerWithIdentifier("home")
+        
+        self.presentViewController(vc, animated: true, completion: nil)
     }
     
 }
